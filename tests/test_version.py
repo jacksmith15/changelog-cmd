ae55fe980dd2a4e481f8a59f73b5ae7bfe58c8e2
Jack Smith <jack.je.smith@gmail.com>
2021-04-16 16:20:39 +0100

    Fix version test

--- tests/test_version.py
+++ tests/test_version.py
@@ -4,13 +4,12 @@ import toml
 import changelog
 
 
-def test_version_matches_pyproject():
+def test_version_matches_pyproject() -> None:
     with open("pyproject.toml", "r") as file:
         pyproject = toml.loads(file.read())
     assert pyproject["tool"]["poetry"]["version"] == changelog.__version__
 
 
-@pytest.mark.xfail(strict=True, reason="No release has yet been made")
-def test_version_matches_changelog():
+def test_version_matches_changelog() -> None:
     log = changelog.load_from_file("CHANGELOG.md")
-    assert log.latest_version == changelog.__version__
+    assert log.latest_tag == changelog.__version__
